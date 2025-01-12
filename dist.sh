411a7e7ed01d7c732fdb0ce6fd3957f3fbf10894
Alexander Nutz <alexandernutz68@gmail.com>
2025-01-13 00:15:13 +0100

    c

---
+++ dist.sh
@@ -0,0 +1,27 @@
+set -e
+if [ -z $1 ]; then
+    echo expected argument: version
+    exit 1
+fi
+
+mkdir downcache-$1
+
+files="$(git ls-files)"
+
+submodule () {
+    files+=" $(cd "$1" && git ls-files | awk '{print "'"$1"'/"$1}')"
+}
+
+submodule "C-Http-Server"
+submodule "allib"
+
+for f in $files; do
+    if [ -f $f ]; then
+        if ! [ -z $(dirname "$f") ]; then
+            mkdir -p downcache-$1/$(dirname "$f")
+        fi
+        cp $f downcache-$1/$(dirname "$f")/
+    fi
+done
+tar -czf downcache-$1.tar.gz downcache-$1
+rm -r downcache-$1
